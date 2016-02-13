857a08bcdcdbc3e00ae9afb4671b79f1d1d82f08
vinayhz <vinaykumar.hz@gmail.com>
2016-02-13 21:03:44 +0530

    Initial commit

--- js/custom/SignupController.js
+++ js/custom/SignupController.js
@@ -1,9 +1,15 @@
 (function(){
     var app = angular.module("myApp");
     
-    var SignupController = function($scope){
-        $scope.welcome = "Welcome to the future..";
+    var SignupController = function($scope, $location, $http){
+        $scope.welcome = "Welcome to the signup page...";
+        
+        $scope.signup = function(user){
+            console.log(user.userName+"::"+user.userEmail+"::"+user.password);
+            
+            $http.post("http://localhost:8080/signup", user);
+        };
     };
     
-    app.controller("SignupController",['$scope',SignupController])
+    app.controller("SignupController",['$scope', '$location', '$http',SignupController])
 })();
